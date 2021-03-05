5afb56de078ae0f58f2e6d42a371a2a68157f7bf
SnowballSH <derek070211@gmail.com>
2021-03-05 14:42:07 -0600

    reeeeeee

--- components/dataInput.tsx
+++ components/dataInput.tsx
@@ -1,13 +1,72 @@
 import {FieldCombo} from "./field";
 import {Component} from "react";
 
-export default class DataInput extends Component<any, any> {
+import autoBind from 'react-autobind';
+
+interface PP {
+    e: JSX.Element
+    xText: string
+    yText: string
+}
+
+export default class DataInput extends Component<{}, PP[]> {
+    constructor(props) {
+        super(props);
+        this.state = [
+            {
+                e: <FieldCombo defaultX="10" defaultY="33"
+                               onChangeX={this.bindChange(0, true)}
+                               onChangeY={this.bindChange(0, false)}/>,
+                xText: "10",
+                yText: "33"
+            },
+            {
+                e: <FieldCombo defaultX="20" defaultY="66"
+                               onChangeX={this.bindChange(1, true)}
+                               onChangeY={this.bindChange(1, false)}/>,
+                xText: "20",
+                yText: "66"
+            },
+            {
+                e: <FieldCombo defaultX="30" defaultY="99"
+                               onChangeX={this.bindChange(1, true)}
+                               onChangeY={this.bindChange(1, false)}/>,
+                xText: "20",
+                yText: "66"
+            }
+        ];
+        autoBind(this)
+    }
+
+    bindChange(i: number, x: boolean): (string) => void {
+        return (newVal: string) => {
+            this.onTextChange(i, x, newVal);
+        }
+    }
+
+    onTextChange(i: number, x: boolean, newVal: string) {
+        let w = [...Object.values(this.state)];
+        if (x) {
+            w[i] = {
+                e: w[i].e,
+                xText: newVal,
+                yText: w[i].yText
+            };
+        } else {
+            w[i] = {
+                e: w[i].e,
+                xText: w[i].xText,
+                yText: newVal
+            };
+        }
+        this.setState(w);
+    }
+
     render() {
         return <div className="space-x-4">
             <br/>
-            <FieldCombo defaultX="10" defaultY="33"/>
-            <FieldCombo defaultX="20" defaultY="66"/>
-            <FieldCombo defaultX="30" defaultY="99"/>
+            {Object.values(this.state).map(e => e.e)}
+            {Object.values(this.state).map(e => <p>{e.xText} {e.yText}</p>)}
         </div>;
     }
 }
--- components/field.tsx
+++ components/field.tsx
@@ -1,10 +1,14 @@
 import {useState, ChangeEvent, Component} from "react";
 import {TextField} from "@material-ui/core";
 
-export function Field(props: { default: string, label: string }) {
+export function Field(props: {
+    default: string, label: string,
+    onChange: (string) => void
+}) {
     const [name, setName] = useState(props.default);
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setName(event.target.value);
+        props.onChange(event.target.value)
     };
 
     return (
@@ -17,12 +21,22 @@ export function Field(props: { default: string, label: string }) {
     );
 }
 
-export class FieldCombo extends Component<{ defaultX: string, defaultY: string }, { x: JSX.Element, y: JSX.Element }> {
-    constructor(props: { defaultX: string, defaultY: string }) {
+interface PP {
+    defaultX: string,
+    defaultY: string,
+    onChangeX: (string) => void
+    onChangeY: (string) => void
+}
+
+export class FieldCombo extends Component<PP,
+    { x: JSX.Element, y: JSX.Element }> {
+    constructor(props: PP) {
         super(props);
         this.state = {
-            x: <a style={{marginRight: "10px"}}><Field default={this.props.defaultX} label={"X"}/></a>,
-            y: <Field default={this.props.defaultY} label={"Y"}/>
+            x: <a style={{marginRight: "10px"}}>
+                <Field default={this.props.defaultX} label={"X"} onChange={props.onChangeX}/>
+            </a>,
+            y: <Field default={this.props.defaultY} label={"Y"} onChange={props.onChangeY}/>
         };
     }
 
