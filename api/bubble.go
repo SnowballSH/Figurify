f8ac5e71f50f453915976be00b4e12b5a8951088
SnowballSH <derek070211@gmail.com>
2021-03-11 22:14:08 -0600

    bubble.go

---
+++ api/bubble.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func Bubble(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	switch r.Method {
+	case http.MethodPost:
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Invalid POST data (No form)", http.StatusBadRequest)
+			return
+		}
+
+		var requestedArray []int
+
+		var resp struct {
+			Array []int
+		}
+
+		d := json.NewDecoder(r.Body)
+
+		err := d.Decode(&resp)
+
+		if err != nil {
+			http.Error(w, "Invalid POST data (Invalid array)\n"+err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		requestedArray = resp.Array
+
+		swapped := true
+
+		var result [][]int
+		length := len(requestedArray)
+
+		k := make([]int, length)
+		copy(k, requestedArray)
+		result = append(result, k)
+
+		for swapped {
+			swapped = false
+			for i := 1; i < length; i++ {
+				if requestedArray[i-1] > requestedArray[i] {
+					requestedArray[i-1], requestedArray[i] = requestedArray[i], requestedArray[i-1]
+					k := make([]int, length)
+					copy(k, requestedArray)
+					result = append(result, k)
+					swapped = true
+				}
+			}
+		}
+
+		//fmt.Println(result)
+
+		marshalled, e := json.Marshal(result)
+		if e != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		_, _ = w.Write(marshalled)
+
+	default:
+		http.Error(w, "Cannot "+r.Method, http.StatusMethodNotAllowed)
+		return
+	}
+
+	duration := time.Since(start)
+
+	fmt.Println(duration)
+}
--- api/svg.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import (
-	"net/http"
-)
-
-func SVG(w http.ResponseWriter, r *http.Request) {
-
-}
