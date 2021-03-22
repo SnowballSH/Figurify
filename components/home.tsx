6224d9975f925266f9dbcc7ae70af29c43bd7091
SnowballSH <derek070211@gmail.com>
2021-03-21 20:01:27 -0500

    home

--- components/home.tsx
+++ components/home.tsx
@@ -33,23 +33,32 @@ export default function HomePage() {
             </div>
 
             <div className="text-center pl-20 pr-20">
-                <Card className="flex">
+                <Card className="p-5">
                     <CardContent>
-                        <Typography variant={"h2"} className="font-bold">
+                        <Typography variant={"h3"} className="font-bold">
                             Visualize 2D Data
                         </Typography>
                     </CardContent>
-                    <Chart options={{
-                        chart: {
-                            id: 'apexchart-example'
-                        },
-                        xaxis: {
-                            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-                        }
-                    }} series={[{
-                        name: 'series-1',
-                        data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-                    }]} type="bar" width={500} height={320}/>
+                    <div style={{display: typeof window !== "undefined" && window.innerWidth > 800 ? "flex" : "inherit"}}>
+                        <div>
+                            <Chart options={{
+                                chart: {
+                                    id: 'apexchart-example'
+                                },
+                                xaxis: {
+                                    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+                                }
+                            }} series={[{
+                                name: 'series-1',
+                                data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+                            }]} type="bar" width={500} height={320}/>
+                        </div>
+                        <CardContent>
+                            <Typography variant={"h5"} className="font-bold">
+                                Figurify allows you to create interactive, downloadable graphs with simple clicks.
+                            </Typography>
+                        </CardContent>
+                    </div>
                 </Card>
             </div>
         </div>
